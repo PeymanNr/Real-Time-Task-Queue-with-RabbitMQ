fe64c377eb88093cc493d61d20adef0d0179081e
PeymanNr <peyman.naseri7@gmail.com>
2024-06-17 21:32:55 +0330

    Implemented RabbitMQ Consumer for message publishing

---
+++ consumer/task_consumer.py
@@ -0,0 +1,26 @@
+import pika
+from rabbit_connection import parameters_rabbit
+
+connection = pika.BlockingConnection(parameters_rabbit)
+channel = connection.channel()
+
+queue_name = 'request_queue'
+
+
+def callback(ch, method, properties, body):
+    print(" [x] Received %r" % body.decode('utf-8'))
+    response = "Hello back!"
+    channel.basic_publish(
+        exchange='',
+        routing_key=properties.reply_to,
+        properties=pika.BasicProperties(correlation_id=properties.correlation_id),
+        body=response.encode('utf-8'))
+
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+
+channel.queue_declare(queue=queue_name)
+channel.basic_consume(queue=queue_name, on_message_callback=callback)
+
+print(' [*] Waiting for requests. To exit, press CTRL+C')
+channel.start_consuming()
