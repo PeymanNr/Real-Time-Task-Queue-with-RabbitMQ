cf956ea4dfa9b44a9c2ff1c72c75409104e9ad44
PeymanNr <peyman.naseri7@gmail.com>
2024-06-17 21:32:30 +0330

    Implemented RabbitMQ Producer for message publishing

---
+++ producer/task_producer.py
@@ -0,0 +1,29 @@
+import pika
+import uuid
+from rabbit_connection import parameters_rabbit
+
+
+def callback(ch, method, properties, body):
+    print(" [x] Received %r" % body.decode('utf-8'))
+
+
+connection = pika.BlockingConnection(parameters_rabbit)
+channel = connection.channel()
+
+request_queue = 'request_queue'
+
+corr_id = str(uuid.uuid4())
+channel.queue_declare(queue=corr_id)
+
+message = 'Hi Friend'
+channel.basic_publish(
+    exchange='', routing_key=request_queue,
+    properties=pika.BasicProperties(reply_to=corr_id),
+    body=message.encode('utf-8'))
+print(" [x] Sent %r" % message)
+
+
+channel.basic_consume(queue=corr_id, on_message_callback=callback)
+
+print(' [*] Waiting for response. To exit, press CTRL+C')
+channel.start_consuming()
