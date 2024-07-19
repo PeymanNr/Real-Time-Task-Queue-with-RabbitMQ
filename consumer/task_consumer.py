0f15ad370a15fe5338546bd2f2f961332c185fad
PeymanNr <peyman.naseri7@gmail.com>
2024-07-19 22:55:39 +0330

    Define a fixed

--- consumer/task_consumer.py
+++ consumer/task_consumer.py
@@ -1,28 +1,30 @@
-import pika
 from utils.rabbit_connection import rabbit_connection
 
 connection = rabbit_connection
 channel = connection.channel()
 
-queue_name = 'request_queue'
+channel.queue_declare(queue='request_queue')
 
 
-def callback(ch, method, properties, body):
-    print(" [x] Received %r" % body.decode('utf-8'))
-    response = "Hello back!"
+def on_reply_message_receive(ch, method, properties, body):
+    print(f"Received request: {properties.correlation_id} ")
     channel.basic_publish(
         exchange='',
         routing_key=properties.reply_to,
-        properties=pika.BasicProperties(
-            correlation_id=properties.correlation_id
-        ),
-        body=response.encode('utf-8'))
+        body=f" Reply to {properties.correlation_id}")
 
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
 
-channel.queue_declare(queue=queue_name)
-channel.basic_consume(queue=queue_name, on_message_callback=callback)
-print(' [*] Waiting for requests. To exit, press CTRL+C')
+channel.basic_consume(queue='request_queue',
+                      on_message_callback=on_reply_message_receive
+                      )
+print('Starting Server..! ')
 channel.start_consuming()
+
+
+
+
+
+print(' [*] Waiting for requests. To exit, press CTRL+C')
 
--- producer/task_producer.py
+++ producer/task_producer.py
@@ -3,32 +3,34 @@ import uuid
 from utils.rabbit_connection import rabbit_connection
 
 
-def callback(ch, method, properties, body):
-    print(" [x] Received %r" % body.decode('utf-8'))
-
-
 connection = rabbit_connection
 channel = connection.channel()
 
-request_queue = 'request_queue'
-reply_queue = 'reply_queue'
+reply_queue = channel.queue_declare(queue='', exclusive=True)
 
+
+def on_reply_message_receive(ch, method, properties, body):
+    print(f'reply received: {body}')
+
+
+channel.basic_consume(
+    queue=reply_queue.method.queue,
+    auto_ack=True, on_message_callback=on_reply_message_receive
+)
+
+channel.queue_declare(queue='request_queue')
 corr_id = str(uuid.uuid4())
+print(f'Sending Request: {corr_id}')
 
-channel.queue_declare(queue=reply_queue)
 
-message = 'Hi Friend'
 channel.basic_publish(
-    exchange='', routing_key=request_queue,
+    exchange='', routing_key='request_queue',
     properties=pika.BasicProperties(
-        reply_to=reply_queue,
+        reply_to=reply_queue.method.queue,
         correlation_id=corr_id
     ),
-    body=message.encode('utf-8'))
-print(" [x] Sent %r" % message)
+    body='Can i request a reply')
 
 
-channel.basic_consume(queue=reply_queue, on_message_callback=callback)
-
-print(' [*] Waiting for response. To exit, press CTRL+C')
+print('starting Client ...')
 channel.start_consuming()
